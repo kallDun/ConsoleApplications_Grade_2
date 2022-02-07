5e7d43b019b11e9682eb48cda81797bdd4aab0ee
kallDun <igorxlonline24@gmail.com>
2022-02-07 23:11:47 +0200

    calculator

---
+++ Lab_2_1_Calculator/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab_2_1_Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        bool isAdditionPanelShow = false;
+        private void burger_show_panel_btn_Click(object sender, RoutedEventArgs e)
+        {
+            isAdditionPanelShow = !isAdditionPanelShow;
+            if (isAdditionPanelShow)
+            {
+                AdditionColumnPanel.Width = new GridLength(1, GridUnitType.Star);
+                burger_show_panel_btn_textblock.Text = "<";
+                burger_show_panel_btn_textblock.FontSize = 35;
+                MinWidth = 310;
+            }
+            else
+            {
+                AdditionColumnPanel.Width = new GridLength(0, GridUnitType.Star);
+                burger_show_panel_btn_textblock.Text = "☰";
+                burger_show_panel_btn_textblock.FontSize = 24;
+                MinWidth = 250;
+            }
+        }
+    }
+}
---
+++ Lab_2_3/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab_2_3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Horse> Horses = new List<Horse>()
+        {
+            new Horse{Name = "Horse_1", Color = Colors.Bisque, Position = 1, Coeff = 1.2, Time = new TimeSpan(0, 0, 20)},
+            new Horse{Name = "Horse_3", Color = Colors.Azure, Position = 2, Coeff = 1.67, Time = new TimeSpan(0, 0, 20)},
+            new Horse{Name = "Horse_5", Color = Colors.DarkBlue, Position = 3, Coeff = 1.33, Time = new TimeSpan(0, 0, 20)},
+            new Horse{Name = "Horse_2", Color = Colors.DarkViolet, Position = 4, Coeff = 2.1, Time = new TimeSpan(0, 0, 20)},
+            new Horse{Name = "Horse_4", Color = Colors.Indigo, Position = 5, Coeff = 1.8, Time = new TimeSpan(0, 0, 20)},
+        };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class Horse
+    {
+        public string Name { get; set; }
+        public Color Color { get; set; }
+        public int Position { get; set; }
+        public TimeSpan Time { get; set; }
+        public double Coeff { get; set; }
+    }
+}
