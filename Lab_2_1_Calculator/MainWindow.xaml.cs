4db649526b55b823a219cde09d1e1897650573fc
kallDun <pc_127_1@outlook.com>
2022-02-14 11:53:00 +0200

    key down calculator update

--- Lab_2_1_Calculator/MainWindow.xaml.cs
+++ Lab_2_1_Calculator/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Globalization;
 using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace Lab_2_1_Calculator
 {
@@ -40,8 +41,11 @@ namespace Lab_2_1_Calculator
         private void Button_Number_Click(object sender, RoutedEventArgs e)
         {
             var character = ((sender as Button).Content as TextBlock).Text;
+            InsertCharacter(character);
+        }
+        private void InsertCharacter(string character)
+        {
             if (character is "." && Number_TextBlock.Text.Contains(".")) return;
-
             if (Number_TextBlock.Text is "0" && character is not "." and not "00" and not "0")
             {
                 Number_TextBlock.Text = character;
@@ -54,8 +58,12 @@ namespace Lab_2_1_Calculator
         private void Button_Operation_Click(object sender, RoutedEventArgs e)
         {
             if (Number_TextBlock.Text[Number_TextBlock.Text.Length - 1] == '.') Number_TextBlock.Text += '0';
+            CalculateOperation((sender as Button).Tag as string);
+        }
+        private void CalculateOperation(string tag)
+        {
             var number = double.Parse(Number_TextBlock.Text);
-            switch ((sender as Button).Tag)
+            switch (tag)
             {
                 case "Plus":
                     client.Plus(number);
@@ -130,6 +138,86 @@ namespace Lab_2_1_Calculator
                     break;
                 case "Exp":
                     Number_TextBlock.Text = string.Format("{0:0.##########}", Math.E);
+                    break;
+            }
+        }
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.D1:
+                case Key.NumPad1:
+                    InsertCharacter("1");
+                    break;
+                case Key.D2:
+                case Key.NumPad2:
+                    InsertCharacter("2");
+                    break;
+                case Key.D3:
+                case Key.NumPad3:
+                    InsertCharacter("3");
+                    break;
+                case Key.D4:
+                case Key.NumPad4:
+                    InsertCharacter("4");
+                    break;
+                case Key.D5:
+                case Key.NumPad5:
+                    InsertCharacter("5");
+                    break;
+                case Key.D6:
+                case Key.NumPad6:
+                    InsertCharacter("6");
+                    break;
+                case Key.D7:
+                case Key.NumPad7:
+                    InsertCharacter("7");
+                    break;
+                case Key.D8:
+                case Key.NumPad8:
+                    InsertCharacter("8");
+                    break;
+                case Key.D9:
+                case Key.NumPad9:
+                    InsertCharacter("9");
+                    break;
+                case Key.D0:
+                case Key.NumPad0:
+                    InsertCharacter("0");
+                    break;
+                case Key.Separator:
+                case Key.OemComma:
+                case Key.OemPeriod:
+                    InsertCharacter(".");
+                    break;
+                case Key.OemMinus:
+                case Key.Subtract:
+                    CalculateOperation("Subtract");
+                    break;
+                case Key.OemPlus:
+                case Key.Add:
+                    CalculateOperation("Plus");
+                    break;
+                case Key.Multiply:
+                    CalculateOperation("Multiply");
+                    break;
+                case Key.Divide:
+                case Key.OemQuestion:
+                    CalculateOperation("Divide");
+                    break;
+                case Key.Enter:
+                    CalculateOperation("Equals");
+                    break;
+                case Key.Back:
+                    Backspace_Button_Click(null, null);
+                    break;
+                case Key.Escape:
+                case Key.Space:
+                    CE_Button_Click(null, null);
+                    break;
+                case Key.Delete:
+                case Key.Home:
+                    C_Button_Click(null, null);
                     break;
             }
         }
