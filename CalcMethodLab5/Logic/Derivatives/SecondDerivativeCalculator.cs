113110c1d8970b36ae97e81b653d2fe4c019e59d
kallDun <igorxlonline24@gmail.com>
2022-05-11 14:01:43 +0300

    lab5 changed method

--- CalcMethodLab5/Logic/Derivatives/SecondDerivativeCalculator.cs
+++ CalcMethodLab5/Logic/Derivatives/SecondDerivativeCalculator.cs
@@ -8,9 +8,9 @@ namespace CalcMethodLab5.Logic.Derivatives
         public override double GetDerivative(double x)
         {
             return
-                (-GetFuncValueOnXGrid(x, -2) + 16 * GetFuncValueOnXGrid(x, -1)
-                - 30 * GetFuncValueOnXGrid(x, 0)
-                + 16 * GetFuncValueOnXGrid(x, 1) - GetFuncValueOnXGrid(x, 2))
+                (-GetFuncValueOnXGrid(x, -2) + (16 * GetFuncValueOnXGrid(x, -1))
+                - (30 * GetFuncValueOnXGrid(x, 0))
+                + (16 * GetFuncValueOnXGrid(x, 1)) - GetFuncValueOnXGrid(x, 2))
                 / (12 * Math.Pow(step, 2));
             /*return (2 * GetFuncValueOnXGrid(x, 0) - 5 * GetFuncValueOnXGrid(x, 1) +
                 4 * GetFuncValueOnXGrid(x, 2) - GetFuncValueOnXGrid(x, 3)) / 
